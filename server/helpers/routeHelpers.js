e38fda252644c3a5f8073ed3f86e160db47d98c6
dov stokar <dov.stokar@gmail.com>
2018-10-26 14:07:59 -0400

    url undefined

--- server/helpers/routeHelpers.js
+++ server/helpers/routeHelpers.js
@@ -68,7 +68,7 @@ module.exports = {
           const result = JSON.parse(body);
           const url = result.instance_url;
           const sfToken = result.access_token;
-          console.log(result);
+          console.log(url);
            request({
             url: `${url}/services/data/v43.0/query?q=select+id,account.name,account.id,account.type,account.products__c+FROM+Contact+WHERE+email+='${email}'`,
             method: 'GET',
